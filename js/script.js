98ad7c514c7b66fd408b0ebc3c936766a808876d
AndreaAbbonizio <andreaabbonizio01@gmail.com>
2023-03-15 19:26:06 +0100

    feat: exercise done with bonus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+//Stampare all'interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+
+// Predisporre un campo di input testuale e un pulsante "aggiungi": 
+// cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo
+// che quindi viene aggiunto alla lista dei todo esistenti.
+
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
+
+
 const { createApp } = Vue
 
   createApp({
@@ -6,7 +20,7 @@ const { createApp } = Vue
         todos: [
             {
                 text : 'Fare ricarica al telefono',
-                done : true ,
+                done : false ,
             },
             {
                 text : 'Portare il cane al parco',
@@ -18,14 +32,37 @@ const { createApp } = Vue
             },
             {
                 text : 'Inviare la consegna del nuovo esercizio',
-                done : true ,
+                done : false ,
             },
             {
                 text : "Chiedere l'età a Danilo" ,
-                done : true ,
+                done : false ,
             },
             
-          ],
+        ],
+        newTodo: '',
+        newItem : {text: '', done: false},
+    
       }
-    }
+    },
+    methods: {
+        deleteTodo(index) {
+          this.todos.splice(index, 1);
+    
+        },
+        addToDo() {
+            this.newItem.text = this.newTodo;
+            this.todos.push(this.newItem);
+            this.newItem = {text: "", done: false};
+            this.newTodo = '';
+            
+        },
+        changeDone(index){
+            if(this.todos[index].done == true){
+                this.todos[index].done = false;
+            }else{
+                this.todos[index].done = true;
+            }
+        }
+    },
   }).mount('#app')
